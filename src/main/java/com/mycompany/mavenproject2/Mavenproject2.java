d5dced4fa6f8db61015c468b591929f03e1f60f7
TECH DEALZ <TECH DEALZ@DESKTOP-MROBLNE.home>
2024-10-29 08:59:43 +0000

    program to add 2 numbers

--- src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 package com.mycompany.mavenproject2;
-//St10446175 saddiq Muhammad poe part 1
+
 import java.util.Scanner;
 
 /**
