67e4e2f8a686e13dd3893e9d5e8f61b154054bdf
TECH DEALZ <TECH DEALZ@DESKTOP-MROBLNE.home>
2024-10-30 09:24:11 +0000

    refix

--- src/main/java/com/mycompany/mavenproject2/Login.java
+++ src/main/java/com/mycompany/mavenproject2/Login.java
@@ -3,62 +3,63 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.mavenproject2;
-//St10446175 saddiq Muhammad poe part 1
+
 /**
  *
  * @author TECH DEALZ
  */
+// Class to handle user login and registration
 class Login {
+    private String username; // Stores the username
+    private String password; // Stores the password
+    private String firstName; // Stores the user's first name
+    private String lastName; // Stores the user's last name
 
-    //method to check username
-     public boolean checkUserName(String username) {
-         
-         if(username.contains("_") && username.length() <= 5){
-             return true;
-             
-         }else{
-             
-             return false;
-             
-         }
-         
-        
+    // Method to check if the username is valid
+    public boolean checkUserName(String username) {
+        // Username must contain an underscore and be no more than 5 characters long
+        return username.contains("_") && username.length() <= 5;
     }
-     
-     //method to check password
-     public boolean checkPasswordComplexity(String password) {
+
+    // Method to check if the password meets complexity requirements
+    public boolean checkPasswordComplexity(String password) {
+        // Password must be at least 8 characters long, contain a capital letter, a number, and a special character
         return password.length() >= 8 &&
-               password.matches(".[A-Z].") &&
-               password.matches(".[0-9].") &&
-               password.matches(".[!@#$%^&()].*");
+               password.chars().anyMatch(Character::isUpperCase) &&
+               password.chars().anyMatch(Character::isDigit) &&
+               password.chars().anyMatch(ch -> "!@#$%^&*()_+[]{}|;':,.<>?/`~".indexOf(ch) >= 0);
     }
-     
-     //register method
-     public String register(String username, String password, String firstName, String lastName){
-         
-         //if check password and username methods
-         if(checkUserName(username) && checkPasswordComplexity(password)){
-             
-             return "";
-         }else{
-             return "";
-         }
-     }
-     
-     //login method
-    public boolean loginUser(String login_username,String username, String login_password, String password) {
-        
-        return login_username.equals(username) && login_password.equals(password);
-    }
-    
-    //login status
-    public String returnLoginStatus(String login_username,String username, String login_password, String password, String firstName, String lastName) {
-        if (loginUser(login_username,username,login_password, password)) {
-            return "Welcome " + firstName + " " + lastName + ", it is great to see you again.";
-        } else {
-            return "Username or password incorrect, please try again";
+
+    // Method to register a new user
+    public String registerUser(String username, String password, String firstName, String lastName) {
+        // Validate username format
+        if (!checkUserName(username)) {
+            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.";
         }
-    
+        // Validate password complexity
+        if (!checkPasswordComplexity(password)) {
+            return "Password does not meet the complexity requirements.";
+        }
+        // Store user details upon successful registration
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        return "Username successfully captured. You are now registered!";
     }
-    
+
+    // Method to verify login credentials
+    public boolean loginUser(String username, String password) {
+        // Check if provided credentials match stored credentials
+        return this.username.equals(username) && this.password.equals(password);
+    }
+
+    // Method to return a message based on login status
+    public String returnLoginStatus(boolean isLoggedIn) {
+        if (isLoggedIn) {
+            return "Welcome " + firstName + " " + lastName + ", it is great to see you again!";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
+    }
 }
